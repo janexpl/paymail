0ec7fe602eed01d9e3883daced095fc5a29e379a
janusz <janexpl@me.com>
2020-05-03 12:07:44 +0200

    commandline fix

--- cmd/main.go
+++ cmd/main.go
@@ -16,30 +16,35 @@ func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 func main() {
+
 	configPtr := flag.String("config", "", "Config path")
 	employeePtr := flag.String("employee", "", "Employee list path")
 	flag.Parse()
-	config, _ := internal.NewConfig(*configPtr)
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d",
-		config.Database.Host,
-		config.Database.Username,
-		config.Database.Password,
-		config.Database.Port)
+	if *configPtr != "" && *employeePtr != "" {
+		config, _ := internal.NewConfig(*configPtr)
+		connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d",
+			config.Database.Host,
+			config.Database.Username,
+			config.Database.Password,
+			config.Database.Port)
 
-	conn, err := sql.Open("mssql", connString)
-	if err != nil {
-		log.Fatal("Open connection failed:", err.Error())
+		conn, err := sql.Open("mssql", connString)
+		if err != nil {
+			log.Fatal("Open connection failed:", err.Error())
+		}
+		defer conn.Close()
+		emp, _ := internal.NewEmployee(*employeePtr)
+		emp.ReadAll()
+
+		payments := internal.NewPayments(conn, emp, config)
+		var wg sync.WaitGroup
+
+		wg.Add(len(emp.Employees))
+		for _, employee := range emp.Employees {
+			go payments.SendEmail(employee.Username, &wg)
+		}
+		wg.Wait()
+	} else {
+		flag.Usage()
 	}
-	defer conn.Close()
-	emp, _ := internal.NewEmployee(*employeePtr)
-	emp.ReadAll()
-
-	payments := internal.NewPayments(conn, emp, config)
-	var wg sync.WaitGroup
-
-	wg.Add(len(emp.Employees))
-	for _, employee := range emp.Employees {
-		go payments.SendEmail(employee.Username, &wg)
-	}
-	wg.Wait()
 }
